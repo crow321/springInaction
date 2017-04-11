2d3efcee4bd130aae84cbebffe8e395af85be261
Crow <Crow@123>
2017-04-11 19:41:20 +0800

    add interface MinderReader

---
+++ src/com/spring/action/idol/MinderReader.java
@@ -0,0 +1,10 @@
+package com.spring.action.idol;
+
+/**
+ * Created by crow on 2017/4/11.
+ */
+public interface MinderReader {
+    public void interruptThought(String thoughts);
+
+    String getThgoughts();
+}
