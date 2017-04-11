f1cb0eaad692774c16f4fd5528784be2bcdf5a65
Crow <Crow@123>
2017-04-11 16:50:50 +0800

    complete aop annotation

--- src/com/spring/action/idol/Audience.java
+++ src/com/spring/action/idol/Audience.java
@@ -1,23 +1,35 @@
 package com.spring.action.idol;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
 
 /**
  * Created by crow on 2017/4/11.
  */
 @Component
+@Aspect
 public class Audience {
+    //定义切点
+    @Pointcut(
+            "execution(* com.spring.action.idol.Performer.perform(..))")
+    public void performance(){
+    }
+    //
+    @Before("performance()")
     public void beforePerform(){
         System.out.println("表演开始前...");
     }
+    @AfterReturning("performance()")
     public void afterPerform(){
         System.out.println("表演结束...");
     }
+    @AfterThrowing("performance()")
     public void throwPerform(){
         System.out.println("表演开始前和表演结束...");
     }
-
+    @Around("performance()")
     public void watchPerform(ProceedingJoinPoint joinPoint) {
 
         try {
